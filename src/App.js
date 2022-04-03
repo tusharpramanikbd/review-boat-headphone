1e39eade13435d60e1688b08eee0bd0dfe78e502
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-03 21:25:05 +0600

    all the route created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import About from './components/About/About'
 import Blogs from './components/Blogs/Blogs'
 import Dashboard from './components/Dashboard/Dashboard'
 import Home from './components/Home/Home'
+import NotFound from './components/NotFound/NotFound'
 import Reviews from './components/Reviews/Reviews'
 
 function App() {
   return (
     <div className='App'>
-      <Home />
-      <Reviews />
-      <Dashboard />
-      <Blogs />
-      <About />
+      <h1>This is Header</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
