d724180239c224896ff251de5d186b7b3ab4843e
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-04 12:27:12 +0600

    added reviews in reviews page

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Review from '../Review/Review'
 import './Reviews.css'
 
 const Reviews = () => {
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    fetch('ReviewData.json')
+      .then((res) => res.json())
+      .then((data) => setReviews(data))
+  }, [])
   return (
     <div>
-      <h1>This is review page</h1>
+      <h1 className='customer-review-title'>What our customers say!</h1>
+      <section className='section-reviews'>
+        {reviews.map((review) => {
+          return <Review key={review.id} review={review} />
+        })}
+      </section>
     </div>
   )
 }
