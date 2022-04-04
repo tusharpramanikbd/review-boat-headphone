1a55b866e79ae3267c5a66234224e8dafa2f99b0
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-04 10:59:52 +0600

    created seperate component for banner and review data added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,12 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      <section className='section-banner'>
-        <article className='banner-container'>
-          <img
-            className='banner-img'
-            src='https://img.freepik.com/free-photo/pink-headphones-wireless-digital-device_53876-96804.jpg?w=740'
-            alt='Headphone'
-          />
-          <div className='banner-info'>
-            <h1>How's The Boat?</h1>
-            <h1>
-              <span>Your Next Headphone</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-              quo numquam nihil omnis reprehenderit libero aliquid atque
-              deleniti soluta quaerat?
-            </p>
-            <a
-              href='https://www.boat-lifestyle.com/'
-              target='_blank'
-              rel='noreferrer'
-              className='btn'
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-      </section>
+      <Banner />
+      <h1 className='customer-review-title'>Customer Reviews(3)</h1>
     </div>
   )
 }
