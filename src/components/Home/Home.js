809ac90010b7305a495cd4e0cf0130cea83bc4c3
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-04 12:11:14 +0600

    added review all btn and added functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Banner from '../Banner/Banner'
 import Review from '../Review/Review'
 import './Home.css'
 
 const Home = () => {
+  const navigate = useNavigate()
+
   const [reviews, setReviews] = useState([])
+
   useEffect(() => {
     fetch('ReviewData.json')
       .then((res) => res.json())
       .then((data) => setReviews(data))
   }, [])
+
+  const handleSeeAllReviews = () => {
+    navigate('/reviews')
+  }
+
   return (
-    <div>
+    <div className='home-container'>
       <Banner />
       <h1 className='customer-review-title'>Customer Reviews(3)</h1>
       <section className='section-reviews'>
@@ -19,6 +28,11 @@ const Home = () => {
           return index < 3 ? <Review key={review.id} review={review} /> : null
         })}
       </section>
+      <div className='see-all-btn-container'>
+        <button onClick={handleSeeAllReviews} className='btn btn-all-reviews'>
+          See All Reviews
+        </button>
+      </div>
     </div>
   )
 }
