4d00953f9b60ba2fc1bcd69939cb91dc61b03b0e
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-04 21:31:29 +0600

    changed some color, added some description on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,27 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-      <h1>This is about page</h1>
+    <div className='section-about'>
+      <h2>This is a Boat Headphone Review Website created with ReactJS.</h2>
+      <ul>
+        <li>
+          <p>
+            1. React router is used to show the content over multiple pages.
+          </p>
+        </li>
+        <li>
+          <p>
+            2. You can see the top 3 review on the homepage and all the reviews
+            on reviews page.
+          </p>
+        </li>
+        <li>
+          <p>3. Recharts is use to show some charts with given data.</p>
+        </li>
+        <li>
+          <p>4. On the blogs page you can find some question answer.</p>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import './NotFound.css'
 const NotFound = () => {
   return (
     <div>
-      <h1>404 Not Found</h1>
+      <h1 style={{ textAlign: 'center' }}>404 Not Found</h1>
     </div>
   )
 }
